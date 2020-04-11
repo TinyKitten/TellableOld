bc8b72132a8ad40fc8afb257c1dcf53bf0128610
TinyKitten <catwalk8205@gmail.com>
2020-04-12 05:21:27 +0900

    react-router-dom設定

--- src/pages/Home/index.test.tsx
+++ src/pages/Home/index.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import { HomeScreen } from '.';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const { getByText } = render(<HomeScreen />);
+  const linkElement = getByText(/Hello Home World!/i);
   expect(linkElement).toBeInTheDocument();
 });
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export const HomeScreen = (): React.ReactElement => <h1>Hello Home World!</h1>;
