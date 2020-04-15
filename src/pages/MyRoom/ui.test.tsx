d7946183997ab13a5b9119bb810b7b994f35778c
TinyKitten <catwalk8205@gmail.com>
2020-04-15 19:27:39 +0900

    マイクミュートのテスト一旦削除

--- src/pages/MyRoom/ui.test.tsx
+++ src/pages/MyRoom/ui.test.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import MyRoomUI from './ui';
-import MicMuteButton from '../../components/MicMuteButton';
-import CallButton from '../../components/CallButton';
 import { testIdSelector } from '../../testutil/testIdSelector';
 
 describe('<MyRoomUI />', () => {
@@ -14,9 +12,7 @@ describe('<MyRoomUI />', () => {
     const onError = jest.fn();
     const wrapper = shallow(
       <MyRoomUI
-        muted={false}
         onHangUp={jest.fn()}
-        toggleLocalMic={jest.fn()}
         remoteUser={mockRemoteUser}
         onError={onError}
         calling
@@ -34,45 +30,12 @@ describe('<MyRoomUI />', () => {
   it('should rendered correctly with not calling state', () => {
     const onError = jest.fn();
     const wrapper = shallow(
-      <MyRoomUI
-        muted={false}
-        onHangUp={jest.fn()}
-        toggleLocalMic={jest.fn()}
-        calling={false}
-        micConnected={false}
-        onError={onError}
-      />,
+      <MyRoomUI onHangUp={jest.fn()} calling={false} micConnected={false} onError={onError} />,
     );
     const callState = wrapper.find(testIdSelector('call-state'));
     const micConnectionError = wrapper.find(testIdSelector('mic-connection-error'));
     expect(callState.text()).toBe('通話していません');
     expect(micConnectionError.text()).toBe('マイクの使用を許可してください。');
     expect(wrapper.exists(testIdSelector('buttons'))).toBe(false);
-  });
-  it('should fire events when buttons clicked', () => {
-    const mockRemoteUser = {
-      displayName: 'remote',
-      uniqueId: 'uid',
-    };
-    const mockOnHangUp = jest.fn();
-    const mockToggleLocalMic = jest.fn();
-    const onError = jest.fn();
-    const wrapper = shallow(
-      <MyRoomUI
-        remoteUser={mockRemoteUser}
-        calling
-        micConnected
-        muted={false}
-        onHangUp={mockOnHangUp}
-        toggleLocalMic={mockToggleLocalMic}
-        onError={onError}
-      />,
-    );
-    const callButton = wrapper.find(CallButton);
-    const micMuteButton = wrapper.find(MicMuteButton);
-    callButton.simulate('click');
-    expect(mockOnHangUp).toHaveBeenCalled();
-    micMuteButton.simulate('click');
-    expect(mockToggleLocalMic).toHaveBeenCalled();
   });
 });
